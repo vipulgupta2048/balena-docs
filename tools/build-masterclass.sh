1692e2d3f14919dee10d746cdac3a3aa36f00ef9
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2025-10-17 16:19:41 +0530

    Making script changes

--- tools/build-masterclass.sh
+++ tools/build-masterclass.sh
@@ -19,27 +19,28 @@ mkdir -p $masterclass_path
 # Debugging Masterclass
 mkdir -p $masterclass_path/debugging/ &
 
-cd pages/learn/accounts && $SCRIPT_DIR/extract-markdown.sh "Accessing your device" <support-access.md >access-device.md && mv access-device.md $masterclass_path/debugging/ &
+cd learn/accounts && $SCRIPT_DIR/extract-markdown.sh "Accessing your device" <support-access.mdx >access-device.md && mv access-device.md $masterclass_path/debugging/ &
 
-cd pages/learn/accounts && $SCRIPT_DIR/extract-markdown.sh "Granting Support Access to a Support Agent" <support-access.md >support-access-device.md && mv support-access-device.md $masterclass_path/debugging/ &
+cd learn/accounts && $SCRIPT_DIR/extract-markdown.sh "Granting Support Access to a Support Agent" <support-access.mdx >support-access-device.md && mv support-access-device.md $masterclass_path/debugging/ &
 
-cd pages/faq/ && $SCRIPT_DIR/extract-markdown.sh "Accessing a Device using a Gateway Device" <debugging-device-gateway.md >device-gateway-partial.md && mv device-gateway-partial.md $masterclass_path/debugging/ &
+cd faq/ && $SCRIPT_DIR/extract-markdown.sh "Accessing a Device using a Gateway Device" <debugging-device-gateway.mdx >device-gateway-partial.md && mv device-gateway-partial.md $masterclass_path/debugging/ &
 
-cd pages/learn/manage && $SCRIPT_DIR/extract-markdown.sh "Device Logs" <device-logs.md >device-logs-partial.md && mv device-logs-partial.md $masterclass_path/debugging/ &
+cd learn/manage && $SCRIPT_DIR/extract-markdown.sh "Device Logs" <device-logs.mdx >device-logs-partial.md && mv device-logs-partial.md $masterclass_path/debugging/ &
 
-cd pages/learn/more/masterclasses/ && $SCRIPT_DIR/extract-markdown.sh "Network debugging" <network-masterclass.md >network.md && mv network.md $masterclass_path/debugging/ &
+# Network masterclass doesn't exist in current structure, commenting out
+# cd learn/masterclasses/ && $SCRIPT_DIR/extract-markdown.sh "Network debugging" <network-masterclass.mdx >network.md && mv network.md $masterclass_path/debugging/ &
 
-cd pages/reference/OS && $SCRIPT_DIR/extract-markdown.sh "Configuring balenaOS" <configuration.md >configuration-partial.md && mv configuration-partial.md $masterclass_path/debugging/ &
+cd reference/OS && $SCRIPT_DIR/extract-markdown.sh "Configuring balenaOS" <configuration.mdx >configuration-partial.md && mv configuration-partial.md $masterclass_path/debugging/ &
 
 # Supervisor part is fetched directly into the directory. Refer fetch-external.sh
 # Engine part is fetched directly into the directory. Refer fetch-external.sh
 
-cd pages/learn/manage && $SCRIPT_DIR/extract-markdown.sh "Device Connectivty states" <device-statuses.md >device-connectivity.md && mv device-connectivity.md $masterclass_path/debugging/ &
+cd learn/manage && $SCRIPT_DIR/extract-markdown.sh "Device Connectivty states" <device-statuses.mdx >device-connectivity.md && mv device-connectivity.md $masterclass_path/debugging/ &
 
-# cd pages/reference/OS && $SCRIPT_DIR/extract-markdown.sh "Journal Logs" <debugging-balenaos.md >journal-logs.md && mv journal-logs.md $masterclass_path/debugging/ &
+# cd reference/OS && $SCRIPT_DIR/extract-markdown.sh "Journal Logs" <debugging-balenaos.mdx >journal-logs.md && mv journal-logs.md $masterclass_path/debugging/ &
 
-# cd pages/reference/OS && $SCRIPT_DIR/extract-markdown.sh "Using the Kernel Logs" <debugging-balenaos.md >kernel-logs.md && mv kernel-logs.md $masterclass_path/debugging/ &
+# cd reference/OS && $SCRIPT_DIR/extract-markdown.sh "Using the Kernel Logs" <debugging-balenaos.mdx >kernel-logs.md && mv kernel-logs.md $masterclass_path/debugging/ &
 
-cd pages/faq/ && $SCRIPT_DIR/extract-markdown.sh "Storage Media Debugging" <debugging-storage-media.md >storage-media.md && mv storage-media.md $masterclass_path/debugging/ &
+cd faq/ && $SCRIPT_DIR/extract-markdown.sh "Storage Media Debugging" <debugging-storage-media.mdx >storage-media.md && mv storage-media.md $masterclass_path/debugging/ &
 
 wait
--- tools/build.sh
+++ tools/build.sh
@@ -8,30 +8,30 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/.."
 
 # Generate Getting Started assets
-node ./tools/generate-docs-contracts.js &
+# node ./tools/generate-docs-contracts.js &
 
 # Generate versioned balena-cli docs
-node ./tools/versioning.js https://github.com/balena-io/balena-cli/blob/master/docs/balena-cli.md &
+# node ./tools/versioning.js https://github.com/balena-io/balena-cli/blob/master/docs/balena-cli.md &
 
-# Generate versioned balena-sdk docs
-node ./tools/versioning.js https://github.com/balena-io/balena-sdk/blob/master/DOCUMENTATION.md &
+# # Generate versioned balena-sdk docs
+# node ./tools/versioning.js https://github.com/balena-io/balena-sdk/blob/master/DOCUMENTATION.md &
 
-# Generate versioned balena-python-sdk docs
-node ./tools/versioning.js https://github.com/balena-io/balena-sdk-python/blob/master/DOCUMENTATION.md &
+# # Generate versioned balena-python-sdk docs
+# node ./tools/versioning.js https://github.com/balena-io/balena-sdk-python/blob/master/DOCUMENTATION.md &
 
 # Generate Masterclasses Dynamically
 ./tools/build-masterclass.sh &
 
 # Generate base images docs
-./tools/build-base-images.sh & 
+# ./tools/build-base-images.sh & 
 
 wait
 
 # Convert .jpg, .jpeg, .png images to .webp format
-./tools/convert-images-to-webp.sh
+# ./tools/convert-images-to-webp.sh
 
-# run cd in a subshell so we don't end up in another directory on failure
-(
-    cd "$SCRIPT_DIR/.."
-    node --unhandled-rejections=strict ./node_modules/.bin/doxx "$(pwd)/config/doxx"
-)
+# # run cd in a subshell so we don't end up in another directory on failure
+# (
+#     cd "$SCRIPT_DIR/.."
+#     node --unhandled-rejections=strict ./node_modules/.bin/doxx "$(pwd)/config/doxx"
+# )
